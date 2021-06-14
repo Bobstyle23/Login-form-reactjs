add8bbc56874dafe623e711e5813588062e2e6d9
Bob Pakhriev <boburpakhriev@gmail.com>
2021-06-14 15:45:31 +0900

    Change app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import Login from "./Login";
 import Register from "./Register";
 
-let userRegistered = false;
+let userRegistered = true;
 
 function App() {
   return (
-    <div className="container">{userRegistered ? <Login /> : <Register />}</div>
+    <div className="container">
+      <h1>Hello </h1>
+      {userRegistered ? <Login /> : <Register />}
+    </div>
   );
 }
 

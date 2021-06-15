ea68aaa4f710077f04d2c46c252cab866334dc5a
Bob Pakhriev <boburpakhriev@gmail.com>
2021-06-15 17:17:24 +0900

    Add custom styles to buttons

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
 import Input from "./Input";
+import { useState } from "react";
 
-class Login extends Component {
-  state = {};
-  render(props) {
-    return (
-      <form className="form">
-        <Input type="text" placeholder="Username" />
-        <Input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    );
+function Login() {
+  const [btnColor, setBtnColor] = useState(false);
+
+  function handleBtnColor() {
+    setBtnColor(true);
   }
-}
 
+  function handleMouseOut() {
+    setBtnColor(false);
+  }
+  return (
+    <form className="form">
+      <Input type="text" placeholder="Username" />
+      <Input type="password" placeholder="Password" />
+      <button
+        style={{ backgroundColor: btnColor ? "black" : "white" }}
+        onMouseOver={handleBtnColor}
+        onMouseOut={handleMouseOut}
+        type="submit"
+      >
+        Login
+      </button>
+    </form>
+  );
+}
 export default Login;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,18 +1,33 @@
 import React, { Component } from "react";
 import Input from "./Input";
+import { useState } from "react";
 
-class Register extends Component {
-  state = {};
-  render(props) {
-    return (
-      <form className="form">
-        <Input type="text" placeholder="Username" />
-        <Input type="password" placeholder="Password" />
-        <Input type="password" placeholder="Confirm Password" />
-        <button type="submit">Register</button>
-      </form>
-    );
+function Register() {
+  const [btnColor, setBtnColor] = useState(false);
+
+  function handleBtnColor() {
+    setBtnColor(true);
   }
+
+  function handleMouseOut() {
+    setBtnColor(false);
+  }
+
+  return (
+    <form className="form">
+      <Input type="text" placeholder="Username" />
+      <Input type="password" placeholder="Password" />
+      <Input type="password" placeholder="Confirm Password" />
+      <button
+        style={{ backgroundColor: btnColor ? "black" : "white" }}
+        onMouseOver={handleBtnColor}
+        onMouseOut={handleMouseOut}
+        type="submit"
+      >
+        Register
+      </button>
+    </form>
+  );
 }
 
 export default Register;

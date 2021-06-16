770a3eafc978782c3cfa9557d3a73f8c98cda120
Bob Pakhriev <boburpakhriev@gmail.com>
2021-06-16 12:45:04 +0900

    Add event handler

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,6 +4,37 @@ import { useState } from "react";
 
 function Register() {
   const [btnColor, setBtnColor] = useState(false);
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: "",
+  });
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+
+    setContact((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+          email: prevValue.email,
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+          email: prevValue.email,
+        };
+      } else if (name === "email") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value,
+        };
+      }
+    });
+  }
 
   function handleBtnColor() {
     setBtnColor(true);
@@ -15,9 +46,21 @@ function Register() {
 
   return (
     <form className="form">
-      <Input type="text" placeholder="Username" />
+      <Input
+        onChange={handleChange}
+        type="text"
+        placeholder="Username"
+        value={contact.fName}
+      />
+      <Input
+        onChange={handleChange}
+        type="email"
+        placeholder="Email"
+        value={contact.email}
+      />
       <Input type="password" placeholder="Password" />
       <Input type="password" placeholder="Confirm Password" />
+
       <button
         style={{ backgroundColor: btnColor ? "black" : "white" }}
         onMouseOver={handleBtnColor}
